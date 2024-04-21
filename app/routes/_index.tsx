62a377e205a197c1dfa10b1ddbae48691ff4f35b
disheng <ongdisheng@gmail.com>
2024-04-21 10:49:19 +0800

    Integrated supabase into project

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,23 @@
-import type { MetaFunction } from "@vercel/remix";
+// import statements
+import { json } from "@remix-run/node"; 
+import { useLoaderData } from "@remix-run/react";
+import supabase from "utils/supabase";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+// loader function
+export const loader = async () => {
+  // retrieve owned pokemons from db
+  const { data } = await supabase.from("Pokemons_Owned").select();
+  return json({ data });
 };
 
 export default function Index() {
+  // get data from loader function
+  const { data } = useLoaderData<typeof loader>();
+
+  // render owned pokemons data
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      {JSON.stringify(data)}
     </div>
   );
 }
