bec7487abc1c15d42620d49d8310f8206925dc07
disheng <ongdisheng@gmail.com>
2024-04-22 10:31:19 +0800

    Added error handling for data request issue

--- app/routes/pokemons.$id.tsx
+++ app/routes/pokemons.$id.tsx
@@ -1,5 +1,9 @@
 // import statements
-import { useLoaderData } from "@remix-run/react";
+import {
+  isRouteErrorResponse,
+  useLoaderData,
+  useRouteError
+} from "@remix-run/react";
 import { json, LoaderFunctionArgs } from "@remix-run/node";
 import client from "~/graphql/client.server";
 import { pokemonDetailsQuery } from "~/graphql/query.server";
@@ -9,9 +13,32 @@ export async function loader({
   params,
 }: LoaderFunctionArgs) {
   // request details data from PokeAPI
-  const data = await client.request(pokemonDetailsQuery, { id: params.id });
+  let data;
+  try {
+    data = await client.request(pokemonDetailsQuery, { id: params.id });
+  } catch {
+    throw new Response("Oh no! Unable to fetch request data from PokeAPI!", {
+      status: 503,
+    });
+  }
   console.log(JSON.stringify(data));
   return json({ data: params.id })
+}
+
+// handle errors from loader function
+export const ErrorBoundary = () => {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        <h1>Error {error.status}</h1>
+        <p>{error.data}</p>
+      </div>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
 }
 
 export default function PokemonDetails() {
