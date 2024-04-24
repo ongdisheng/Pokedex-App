cce8cc39a92facb4c09efc5d041b319d6e999e48
disheng <ongdisheng@gmail.com>
2024-04-25 07:53:23 +0800

    Added form component

--- app/components/custom/navbar.tsx
+++ app/components/custom/navbar.tsx
@@ -1,9 +1,12 @@
 // import statements
-import { Link } from "@remix-run/react";
+import { Form, Link, useSubmit } from "@remix-run/react";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 
 export const NavBar = () => {
+  // trigger form submit whenever input value changes
+  const submit = useSubmit();
+
   return (
     <nav className="bg-gray-800 py-2 flex justify-center">
       <div className="flex justify-center items-center w-2/5 sm:w-1/2">
@@ -17,10 +20,15 @@ export const NavBar = () => {
           </Link>
         </div>
         <div className="ml-8 sm:block flex-grow text-center">
-          <div className="mx-auto flex items-center justify-center space-x-2">
-            <Input placeholder="Type Pokemon Name" />
+          <Form
+            onChange={event => {
+              submit(event.currentTarget);
+            }}
+            className="mx-auto flex items-center justify-center space-x-2"
+          >
+            <Input name="pokemonName" placeholder="Type Pokemon Name" />
             <Button className="bg-blue-600 hover:bg-blue-500" type="submit">Search</Button>
-          </div>
+          </Form>
         </div>
       </div>
     </nav>
