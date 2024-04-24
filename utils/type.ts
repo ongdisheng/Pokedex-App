504296e3ab38d5861d2ac769d77405340420323e
disheng <ongdisheng@gmail.com>
2024-04-25 07:53:01 +0800

    Added BasicPokemonData type

--- utils/type.ts
+++ utils/type.ts
@@ -1,6 +1,11 @@
+// import statements
+import { PokemonData } from "./interface";
+
 // form data type
 export type FormData = {
   id: string,
   name: string,
   action: string
 }
+
+export type BasicPokemonData = Partial<PokemonData>;
