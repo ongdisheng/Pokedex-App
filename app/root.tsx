7700086492f26fe832df449ebe6ce4ec816ae3e1
disheng <ongdisheng@gmail.com>
2024-04-25 07:53:41 +0800

    Added loader function

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,41 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  json,
 } from "@remix-run/react";
 import { Analytics } from "@vercel/analytics/react";
 import { NavBar } from "./components/custom/navbar";
 import styles from "./tailwind.css?url";
+import client from "~/graphql/client.server";
+import { pokemonQuery } from "./graphql/query.server";
+import { BasicPokemonData } from "utils/type";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
 ];
+
+// loader function
+export async function loader({
+  request,
+}: LoaderFunctionArgs) {
+  // retrieve pokemon name
+  const pokemonName = new URL(request.url)
+    .searchParams
+    .get("pokemonName");
+
+  // query pokemons with given name
+  const pokemonData: BasicPokemonData = await client.request(
+    pokemonQuery, 
+    { namePrefix: `${pokemonName ? pokemonName + "%" : ""}` }
+  );
+  
+  const data = pokemonData.pokemon_v2_pokemon;
+  return json({ data });
+};
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
