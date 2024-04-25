0290a78894250dff1bf0ab5cfc1c7e89331df4ed
disheng <ongdisheng@gmail.com>
2024-04-25 12:38:36 +0800

    Added pokemon list display below search bar

--- app/components/custom/navbar.tsx
+++ app/components/custom/navbar.tsx
@@ -1,11 +1,30 @@
 // import statements
-import { Form, Link, useSubmit } from "@remix-run/react";
+import { Link } from "@remix-run/react";
+import { ChangeEvent, useState } from "react";
+import { BasicPokemonData } from "utils/type";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
+import { Typography } from "antd";
+import client from "~/graphql/client";
+import { pokemonQuery } from "~/graphql/query";
+const { Text } = Typography;
 
 export const NavBar = () => {
-  // trigger form submit whenever input value changes
-  const submit = useSubmit();
+  const [inputVal, setInputVal] = useState("");
+  const [pokemonList, setPokemonList] = useState<{ id: number, name: string }[]>([]);
+
+  // event handler for input change
+  const handleChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const pokemonName = event.target.value;
+    setInputVal(pokemonName);
+
+    // query pokemons with given name
+    const pokemonData: BasicPokemonData = await client.request(
+      pokemonQuery,
+      { namePrefix: `${pokemonName ? pokemonName + "%" : ""}` }
+    );
+    setPokemonList(pokemonData.pokemon_v2_pokemon!);
+  }
 
   return (
     <nav className="bg-gray-800 py-2 flex justify-center">
@@ -20,15 +39,28 @@ export const NavBar = () => {
           </Link>
         </div>
         <div className="ml-8 sm:block flex-grow text-center">
-          <Form
-            onChange={event => {
-              submit(event.currentTarget);
-            }}
-            className="mx-auto flex items-center justify-center space-x-2"
-          >
-            <Input name="pokemonName" placeholder="Type Pokemon Name" />
+          <div className="mx-auto flex items-center justify-center space-x-2">
+            <div className="flex-grow flew-col">
+              <Input
+                name="pokemonName"
+                placeholder="Type Pokemon Name"
+                value={inputVal}
+                onChange={handleChange}
+              />
+              <div className="bg-white rounded">
+                {pokemonList.map(p => (
+                  <Link key={p.id} to={`/pokemons/${p.id}`}>
+                    <div className="p-2 hover:bg-gray-100">
+                      <Text>
+                        {p.name}
+                      </Text>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </div>
             <Button className="bg-blue-600 hover:bg-blue-500" type="submit">Search</Button>
-          </Form>
+          </div>
         </div>
       </div>
     </nav>
